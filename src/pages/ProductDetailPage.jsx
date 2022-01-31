8abbe410ee19747c7dfeb84e005cdec9a40917b4
= <=>
2022-01-31 14:16:49 -0300

    DetailPage

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,24 +1,29 @@
 import ItemDetail from "../components/ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom"
 import {useEffect, useState} from "react"
+import { getProducts } from "../product"
 
 
 const ItemDetailPage = () => {
     const {id} = useParams()
     const [products, setProducts] = useState([])
+    const [prodFilter, setProdFilter] = useState([])
 
 useEffect(() =>{
-    const URL = `http://localhost:3001/productos/${id}`
-    fetch(URL)
-    .then((res) => res.json())
+    getProducts()
     .then((data) => setProducts(data))
+    console.log(products)
     
-}, [id])
+    const result = products.filter(product => product.id === Number((id)))
+    console.log(result)
 
+    setProdFilter(result)
+    console.log(prodFilter)
+
+}, [id])
     return(
         <>
-        <h1>{products.name}</h1>
-        <ItemDetail product={products}/>
+        <ItemDetail product={prodFilter[0]}/>  
         </>
     )
 }
